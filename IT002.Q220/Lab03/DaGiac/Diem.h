d038781f8422c709026a4842a9f9fe6031ffc7e5
harryhoang <hoang.ghung.forwork@gmail.com>
2026-05-18 14:39:56 +0700

    Add files via upload

---
+++ IT002.Q220/Lab03/DaGiac/Diem.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Diem
+{
+private:
+	double x, y;
+public:
+	Diem(int a = 1, int b = 1) : x(a), y(b) {} // Initialzer list để khởi tạo giá trị mặc định cho các điểm
+	double KhoangCachDiem(); // Các hàm cơ bản cho việc tính toán.
+
+	void NhapDiem();
+	void XuatDiem();
+	
+	void TinhTienDiem(double vectox, double vectoy);
+	double KhoangCachDiem(Diem a);
+	void QuayDiem(double angle);
+
+	void PhongToDiem(double k);
+	void PhongNhoDiem(double k);
+
+	double GetX()
+	{
+		return x;
+	}
+	double GetY()
+	{
+		return y;
+	}
+
+};
+
+
---
+++ IT002.Q220/Lab03/DaGiac/lab03_da_giac.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+#include <vector>
+#include "Diem.h"
+#include "cDaGiac.h"
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cout << "Nhap so luong da giac: "; cin >> n;
+    cDaGiac* DaGiac = new cDaGiac[n]; // Khởi tạo, quản lý thủ công thông qua new...
+    cout << "-----------------Nhap thong tin cho tung Da Giac:---------------- " << endl; //Hiển thị gạch cho dễ nhìn
+    for (int i = 0; i < n; i++)
+    {
+        DaGiac[i].Nhap();
+        
+    }
+    
+    cout << "-----------------Xuat thong tin cho cac Da Giac:------------------ " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "------------------Thong tin da giac: " << i + 1 << endl;
+        DaGiac[i].Xuat();
+        DaGiac[i].TinhChuVi();
+        DaGiac[i].TinhDienTich();
+
+        DaGiac[i].TinhTien(1, 1);
+        cout << "Toa do Da Giac sau khi tinh tien boi vector(1,1) la: " << endl;
+        DaGiac[i].Xuat();
+
+        DaGiac[i].PhongTo(2);
+        cout << "Toa do Da Giac sau khi phong to boi he so k = 2 la: " << endl;
+        DaGiac[i].Xuat();
+
+        DaGiac[i].ThuNho(2);
+        cout << "Toa do Da Giac sau khi thu nho boi he so k = 1.5 la: " << endl;
+        DaGiac[i].Xuat();
+
+        DaGiac[i].Quay(30);
+        cout << "Toa do Da Giac sau khi quay 30 do la: " << endl;
+        DaGiac[i].Xuat();
+
+        DaGiac[i].PhanLoai();
+
+        
+    }
+    delete [] DaGiac; // Hỷ bỏ mảng cấp phát 
+
+}
